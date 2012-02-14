208849ae69f04782fb5d4b10de8c6dc365e36f64
vor89 <ryan.alan.vo@gmail.com>
2012-02-14 12:10:57 -0800

    Update 201202121727.cpp

--- 201202121727.cpp
+++ 201202121727.cpp
@@ -1,3 +1,47 @@
-void funA(void){
-  return;
-}
+/*
+ * rationalNumbers.h
+ *
+ *  Created on: Nov 3, 2011
+ *      Author: chocobogummy
+ */
+
+#ifndef RATIONALNUMBERS_H_
+#define RATIONALNUMBERS_H_
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+
+class RationalNumber{
+public:
+  // Constructor Declaration
+	RationalNumber();
+	RationalNumber(int n);
+	RationalNumber(int num, int den);
+
+	// Member function Declaration
+	RationalNumber add(const RationalNumber &r) const;
+	RationalNumber sub(const RationalNumber &r) const;
+	RationalNumber mul(const RationalNumber &r) const;
+	RationalNumber div(const RationalNumber &r) const;
+	RationalNumber neg(void);
+	bool less(const RationalNumber &r) const;
+	void input(istream &stream);
+	void output(ostream &stream) const;
+
+
+private:
+	// Variable Declaration
+	int a;
+	int b;
+
+	// Member function Declaration
+	void fixNeg(void);
+	int gcd(void) const;
+	void makeNeat(void);
+	void checkRationalNumber(void) const;
+};
+
+#endif /* RATIONALNUMBERS_H_ */
