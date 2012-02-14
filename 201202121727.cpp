5ae425f856b14fd1d0f1411e7cc128a46adcf36d
vor89 <ryan.alan.vo@gmail.com>
2012-02-14 12:02:59 -0800

    Update 201202121727.cpp

--- 201202121727.cpp
+++ 201202121727.cpp
@@ -1,10 +1,3 @@
-17:28 			2012 02 12 17:45 //this is a complicated change
-2012 02 12 17:33 //conflicting edit from vor89
-2012 02 12 17:34 // conflicting edit from vor55
-2012 02 12 17:50 // conflicting edit from vor55
-2012 02 12 18:15 // conflicting edit from vor55
-2012 02 12 20:53 // conflicting edit from vor89
-2012 02 12 20:57 // conflicting edit from vor55
-2012 02 12 21:10 // conflicting edit from vor89
-2012 02 12 21:20 // conflicting edit from vor55
-2012 02 12 21:22 // conflicting edit from vor89
+void funA(void){
+  return;
+}
